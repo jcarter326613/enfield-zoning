ba5347d27a2de9e9b68aeb36ea28822757e34756
Jason Carter <jcarter@naturalhues.com>
2026-04-16 21:25:05 -0400

    Fixed event name and removed header overrides

--- website-api/src/service.ts
+++ website-api/src/service.ts
@@ -18,37 +18,14 @@ const loggerOptions: expressWinston.LoggerOptions = {
 
 export class Service {
     readonly app: Express
-    private readonly allowedMethods = ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"]
 
     constructor(routes: (new (app: express.Application) => RouteBase<any>)[]) {
         this.app = express()
-        this.app.use((req, res, next) => {
-            if (!this.allowedMethods.includes(req.method)) {
-                res.status(405)
-                return res.end('Method Not Allowed')
-            }
-            return next()
-        })
         this.app.use(cookieParser())
         this.app.get('/*', function(req, res, next){ 
             res.setHeader('Last-Modified', (new Date()).toUTCString());
             next(); 
         });
-
-        const crossOrigin = process.env.CROSS_ORIGIN_SOURCE ?? "https://enfieldnhzoning.org"
-        console.debug(`Using CORS targets ${crossOrigin}`)
-        const corsTargets = crossOrigin.split(",")
-
-        const allowedMethodsString = this.allowedMethods.filter(x => x != "OPTIONS").join(",")
-        this.app.use((req, res, next) => {
-            const providedOrigin = req.headers.origin ?? ""
-            const origin = corsTargets.includes(providedOrigin.toLowerCase()) ? providedOrigin : corsTargets[0]
-            res.header("Access-Control-Allow-Origin", origin)
-            res.header("Access-Control-Allow-Headers", "Content-Type")
-            res.header("Access-Control-Allow-Methods", allowedMethodsString)
-            res.header("Access-Control-Allow-Credentials", "true")
-            return next()
-        })
 
         this.app.use(express.json())
         this.app.use(expressWinston.logger(loggerOptions))
