5ba17d96c90021b7096e69dfc2a3bd72ff8c337f
Jason Carter <jcarter@naturalhues.com>
2026-07-02 13:43:49 -0400

    Fixing names for stack

--- aws-deployment/bin/aws-deployment.ts
+++ aws-deployment/bin/aws-deployment.ts
@@ -4,8 +4,9 @@ import { WebsiteApiStack } from "../lib/website-api-stack"
 
 const app = new cdk.App()
 const deployment = app.node.tryGetContext("deployment") ?? "dev"
+const deploymentName = deployment == "prod" ? "WebsiteApiStack-prod" : "WebsiteApiStack"
 
-new WebsiteApiStack(app, `WebsiteApiStack-${deployment}`, {
+new WebsiteApiStack(app, deploymentName, {
     deployment,
     env: {
         account: process.env.CDK_DEFAULT_ACCOUNT,
